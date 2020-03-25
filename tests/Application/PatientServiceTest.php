69a48ef573f8f3c739d174ad96db6929228ea909
nikitades <nikitades@pm.me>
2020-03-26 01:54:29 +0300

    WIP: patientService TDD

--- tests/Application/PatientServiceTest.php
+++ tests/Application/PatientServiceTest.php
@@ -16,6 +16,123 @@ class PatientServiceTest extends TestCase
         return new PatientService();
     }
 
+    private function createAddPatientDTO($ownerName, $dogName, $dogSpecies, $dogBirthDate): CreatePatientDTO
+    {
+        return new CreatePatientDTO(
+            $ownerName,
+            [
+                $dogName,
+                $dogSpecies,
+                $dogBirthDate,
+            ]
+        );
+    }
+
+    public function testGetAllPatients()
+    {
+        $patientService = $this->createPatientService();
+
+        $petName = "Pegasus";
+        $createPatientDTO = $this->createAddPatientDTO("Hercules", $petName, $petName, "1984-01-01");
+        $patientService->addPatient($createPatientDTO);
+
+        $getAllPatientsDTO = new GetAllPatientsDTO();
+        $allPatients = $patientService->getAll($getAllPatientsDTO);
+        static::assertIsArray($allPatients);
+        static::assertCount(1, $allPatients);
+        $allPetsNames = array_map(fn ($pet) => $pet->getName()->getValue(), $allPatients);
+        static::assertContains($petName, $allPatients);
+    }
+
+    public function testGetAllPatientsOnTreatment()
+    {
+        $patientService = $this->createPatientService();
+
+        $patient1 = [
+            'ownerName' => 'Hercules',
+            'name' => 'Pegasus',
+            'species' => 'Pegasus',
+            'birthDate' => '1984-01-01'
+        ];
+
+        $patient2 = [
+            'ownerName' => 'Marc Aurelius',
+            'name' => 'Boulevard',
+            'species' => 'Horse',
+            'birthDate' => '1970-03-20'
+        ];
+
+        $patient3 = [
+            'ownerName' => 'Harry Potter',
+            'name' => 'Ponce',
+            'species' => 'Hyppogryphus',
+            'birthDate' => '1991-09-19'
+        ];
+
+        foreach ([$patient1, $patient2, $patient3] as $patient) {
+            $addPatientDTO = $this->createAddPatientDTO($patient['ownerName'], $patient['name'], $patient['species'], $patient['birthDate']);
+            $patientService->addPatient($addPatientDTO);
+        }
+
+        $releasePatientDTO = new ReleasePatientDTO(
+            $patient2['ownerName'],
+            $patient2['name']
+        );
+        $patientService->releasePatient($releasePatientDTO);
+
+        $getAllPatientsDTO = GetAllPatientsDTO::onTreatment();
+        $allPatients = $patientService->getAll($getAllPatientsDTO);
+        static::assertIsArray($allPatients);
+        static::assertCount(1, $allPatients);
+        $allPetsNames = array_map(fn ($pet) => $pet->getName()->getValue(), $allPatients);
+        static::assertContains($patient2['name'], $allPatients);
+    }
+
+    public function testGetAllReleasedPatients()
+    {
+        $patientService = $this->createPatientService();
+
+        $patient1 = [
+            'ownerName' => 'Hercules',
+            'name' => 'Pegasus',
+            'species' => 'Pegasus',
+            'birthDate' => '1984-01-01'
+        ];
+
+        $patient2 = [
+            'ownerName' => 'Marc Aurelius',
+            'name' => 'Boulevard',
+            'species' => 'Horse',
+            'birthDate' => '1970-03-20'
+        ];
+
+        $patient3 = [
+            'ownerName' => 'Harry Potter',
+            'name' => 'Ponce',
+            'species' => 'Hyppogryphus',
+            'birthDate' => '1991-09-19'
+        ];
+
+        foreach ([$patient1, $patient2, $patient3] as $patient) {
+            $addPatientDTO = $this->createAddPatientDTO($patient['ownerName'], $patient['name'], $patient['species'], $patient['birthDate']);
+            $patientService->addPatient($addPatientDTO);
+        }
+
+        $releasePatientDTO = new ReleasePatientDTO(
+            $patient2['ownerName'],
+            $patient2['name']
+        );
+        $patientService->releasePatient($releasePatientDTO);
+
+        $getAllPatientsDTO = GetAllPatientsDTO::released();
+        $allPatients = $patientService->getAll($getAllPatientsDTO);
+        static::assertIsArray($allPatients);
+        static::assertCount(2, $allPatients);
+        $allPetsNames = array_map(fn ($pet) => $pet->getName()->getValue(), $allPatients);
+        static::assertContains($patient1['name'], $allPatients);
+        static::assertContains($patient3['name'], $allPatients);
+    }
+
     public function testAddAndGetPatient()
     {
         $patientService = $this->createPatientService();
@@ -25,15 +142,7 @@ class PatientServiceTest extends TestCase
         $dogSpecies = "Dog";
         $dogBirthDate = "2000-06-30";
 
-        $createPatientDTO = new CreatePatientDTO(
-            $ownerName,
-            [
-                $dogName,
-                $dogSpecies,
-                $dogBirthDate,
-            ]
-        );
-
+        $createPatientDTO = $this->createAddPatientDTO($ownerName, $dogName, $dogSpecies, $dogBirthDate);
         $patientService->addPatient($createPatientDTO);
 
         $getPatientDTO = new GetPatientDTO(
@@ -49,5 +158,41 @@ class PatientServiceTest extends TestCase
         static::assertEquals($dogBirthDate, $patient->getBirthDate()->getValue()->format("Y-m-d"));
     }
 
-    //TODO: test delete patient, test get all patients, test get all patients on treatment, test get all released patients
+    public function testDeletePatient()
+    {
+        $patientService = $this->createPatientService();
+
+        $ownerName = "Owen Wilson";
+        $dogName = "Marley";
+        $dogSpecies = "Dog";
+        $dogBirthDate = "2000-06-30";
+
+        $addPatientDTO = new AddPatientDTO(
+            $ownerName,
+            [
+                $dogName,
+                $dogSpecies,
+                $dogBirthDate,
+            ]
+        );
+        $patientService->addPatient($addPatientDTO);
+
+        $getAllPatientsDTO = new GetAllPatientsDTO();
+        $allPatients = $patientService->getAll($getAllPatientsDTO);
+        $allPatientsNames = array_map(fn ($patient) => $patient->getName()->getValue(), $allPatients);
+        static::assertContains($dogName, $allPatientsNames);
+
+        $removePatientDto = new RemovePatientDTO(
+            $ownerName,
+            $dogName
+        );
+        $patientService->removePatient($removePatientDto);
+
+        $getAllPatientsDTO = new GetAllPatientsDTO();
+        $allPatients = $patientService->getAll($getAllPatientsDTO);
+        $allPatientsNames = array_map(fn ($patient) => $patient->getName()->getValue(), $allPatients);
+        static::assertNotContains($dogName, $allPatientsNames);
+    }
+
+    //TODO: implement the service!
 }
